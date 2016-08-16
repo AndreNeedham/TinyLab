afc0c1050bcb0a721fe3459dd180d4ed763593d3
AndreNeedham <andre_needham@hotmail.com>
2016-08-16 15:30:38 -0700

    Add files via upload

---
+++ EncoderInterrupt/EncoderInterrupt.ino
@@ -0,0 +1,53 @@
+// Rotary encoder test using 500hz interrupt to poll the encoder
+// Andre Needham
+
+// libraries
+// library can be found at https://github.com/lincomatic/LiquidTWI2
+#include <LiquidTWI2.h>
+// library can be found at https://github.com/mathertel/RotaryEncoder
+#include <RotaryEncoder.h>
+// library can be found at https://github.com/PaulStoffregen/TimerOne
+#include <TimerOne.h>
+
+// instances
+LiquidTWI2 lcd(0x20);
+RotaryEncoder encoder(7, 6);
+
+int8_t rotary_pos = 0;
+volatile int8_t new_pos = 0;
+
+void setup()
+{
+  digitalWrite(4, HIGH); //turn off SD light, I see it flickering/on sometimes
+
+	// LCD setup
+	lcd.setMCPType(LTI_TYPE_MCP23008);
+	lcd.begin(16, 2);
+	lcd.setBacklight(HIGH);
+  lcd.setCursor(0, 0);
+  lcd.print("Encoder position");
+
+  // Timer1 setup
+  Timer1.initialize(2000);
+  Timer1.attachInterrupt(readEncoder); //readEncoder to run every .002 seconds, or 500 times per second
+}
+
+// check the encoder position and store the result
+void readEncoder()
+{
+  encoder.tick();
+  new_pos = encoder.getPosition();
+}
+
+void loop()
+{  
+ 	// Rotary Encoder Test
+	if (rotary_pos != new_pos)
+	{
+    rotary_pos = new_pos;
+    lcd.setCursor(0, 1);
+    lcd.print(rotary_pos);
+    lcd.print("                ");
+	}
+}
+
--- EncoderInterrupt/README.md
+++ EncoderInterrupt/README.md
@@ -1,1 +1,9 @@
+Encoder test using a timer interrupt to poll 500 times/second
 
+Andre Needham
+
+The TinyLab's rotary encoder can be rotated very quickly, and either of the two signals from it can end up being enabled for a very short time.  Since the only way to read the encoder is via polling (only one of the two pins the encoder is using allows for pin change interrupts), it's very possible to miss encoder changes.  The solution is to set up a timer that calls our encoder check function every .002 second (500 times per second).  In the loop code, we can just check to see if our variable changed, and display the current encoder value.
+
+To compile the code you'll need to get the TimerOne library from https://github.com/PaulStoffregen/TimerOne .
+
+I tried using this timer code in TinyLabs' tinylab_test_code project, and it worked there, and didn't seem to interfere with any other tests (though I didn't test with bluetooth/wifi/zigbee/NRF).
